41779a109d0508d687711dbd11c06bce8b26bc93
Aman Kumar <91332159+amankumar000@users.noreply.github.com>
2022-05-20 11:44:54 +0530

    Create task5.py

---
+++ task5.py
@@ -0,0 +1,13 @@
+import numpy as np
+n,m = map(int,input().split())
+m1 = []
+
+for i in range(n):
+    k = list(map(int,input().split()))
+    m1.append(k)
+    
+print(np.transpose(m1))
+
+n = np.array(m1)
+
+print(n.flatten())
