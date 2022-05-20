6206b684edefb8b12fe37fb9fd6f25dd2489d8b7
Aman Kumar <91332159+amankumar000@users.noreply.github.com>
2022-05-20 11:33:16 +0530

    Update task4.py

--- task4.py
+++ task4.py
@@ -1,4 +1,19 @@
+ 
 import numpy as np
 r,c = map(int,input().split())
-matrix = np.eye(r,c)
-print(matrix)
+m1 = []
+m2 = []
+for i in range(r):
+    ele = list(map(int,input().split()))
+    m1.append(ele)
+for i in range(r):
+    ele = list(map(int,input().split()))
+    m2.append(ele)
+n1 = np.array(m1)
+n2 = np.array(m2)
+
+print(np.add(n1,n2))
+print(np.subtract(n1,n2))
+print(np.multiply(n1,n2))
+print(np.floor_divide(n1,n2))
+print(np.mod(n1,n2))
